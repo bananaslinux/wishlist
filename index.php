f4ec9e154ab96c185250ad21c39316633ec3248c
bananaslinux <78407684+bananaslinux@users.noreply.github.com>
2021-02-06 20:45:04 +0100

    Update index.php
    Added santa and background colour

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Wishlist Application</title>
     </head>
-    <body>
-        <form action="wishlist.php" method="GET" name="WishList">
+    <body style="background-color:#de2f32;>
+       <form action="wishlist.php" method="GET" name="WishList">
             Show wish list of: <input type="text" name="user" value=""/>
-            <input type="submit" value="Go" />
+            <input type="submit" value="check" />
+        <div>
+<div style="font-size:20rem;width:100%;text-align:center;">🎅</div>
         </form>
     </body>
 </html>
