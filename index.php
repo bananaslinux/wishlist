9b52f9aa0abe40a12816fa8cbfbe421e497b4d6c
bananaslinux <78407684+bananaslinux@users.noreply.github.com>
2021-02-08 21:33:59 +0100

    Update index.php
    updated with css giggling santa with wishlist.css

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
     <head>
+	<link href="wishlist.css" type="text/css" rel="stylesheet" media="all" />
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Wishlist Application</title>
     </head>
-    <body style="background-color:#de2f32;>
-       <form action="wishlist.php" method="GET" name="WishList">
+    <body>
+<div class="window">
+    <div class="santa">
+      <div class="head">
+        <div class="face">
+          <div class="redhat">
+            <div class="whitepart"></div>
+            <div class="redpart"></div>
+            <div class="hatball"></div>
+          </div>
+          <div class="eyes"></div>
+          <div class="beard">
+            <div class="nouse"></div>
+            <div class="mouth"></div>
+          </div>
+        </div>
+        <div class="ears"></div>
+      </div>
+      <div class="body"></div>
+    </div>
+  </div>
+  <div class="message">
+    <h1>Merry Christmas!</h1>
+<div class="message">
+<h2><form action="wishlist.php" method="GET" name="wishList">
             Show wish list of: <input type="text" name="user" value=""/>
-            <input type="submit" value="check" />
+            <input type="submit" value="check" /></h2>
         <div>
-<div style="font-size:20rem;width:100%;text-align:center;">🎅</div>
         </form>
     </body>
 </html>
